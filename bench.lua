e8bfe71770ad77e738c8d47f4ea20ae23ca9c812
GitSparTV <developspartv@gmail.com>
2019-10-16 23:26:48 +0300

    All benchmarks were redone to use maximums as comparison values

--- bench.lua
+++ bench.lua
@@ -1,6 +1,6 @@
 -- Utils
 function topf(num)
-	return (string.format("%f", num):gsub("(%.-)0+$", ""))
+	return (string.format("%.10f", num):gsub("(%.-)0+$", ""))
 end
 
 function math.mean(t)
@@ -14,48 +14,47 @@ function math.mean(t)
 end
 
 -- Setup
-local name1, name2 = "*", "^"
+local name1, name2 = "1", "2"
 local hr = string.rep("-", 36)
 local rt1, rt2 = {}, {}
 local clock = os.clock
 -- For benchmarking
-local text = "Hello, this is an example text"
-local func = function(s) local x = s end
 
-function A(int)
-	string.gsub(text,".",func)
-end
 
-function B(int)
-	for k=1,#text do local x = text:sub(k,k) end
-end
-
--- Functions
+----------------------
 function test1(arg)
-	local y = A(n)
 end
 
 function test2(arg)
-	local y = B(n)
 end
 
 -- Warmup
 print("Warming up...")
 
 for warm = 1, 1000000 do
-	test1() clock()
+	test1(warm)
+	clock()
 end
 
 for warm = 1, 1000000 do
-	test2() clock()
+	test2(warm)
+	clock()
 end
 
--- os.execute("sleep 1")
+collectgarbage()
+collectgarbage()
+collectgarbage("stop")
+collectgarbage("setpause", 0)
+collectgarbage("setstepmul", 1000)
 collectgarbage()
 collectgarbage()
 local T = 0
-while true do T = T + 1 if T > 1000000 then break end end 
-collectgarbage()
+
+while true do
+	T = T + 1
+	if T > 1000000 then break end
+end
+
 -- Start benchmarking
 print("Benchmarking...")
 print("Benchmarking \"" .. name1 .. "\"...")
@@ -65,11 +64,13 @@ for take = 1, 100 do
 
 	---
 	for times = 1, 1000000 do
-		test1()
+		test1(times)
 	end
 
 	---
 	local END = clock()
+	collectgarbage()
+	collectgarbage()
 	rt1[take] = END - START
 end
 
@@ -80,21 +81,25 @@ for take = 1, 100 do
 
 	---
 	for times = 1, 1000000 do
-		test2()
+		test2(times)
 	end
 
 	---
 	local END = clock()
+	collectgarbage()
+	collectgarbage()
 	rt2[take] = END - START
 end
 
 print(hr) -- Horizontal line
-local r1, r2 = math.mean(rt1), math.mean(rt2)
-local min, max = math.min(r1, r2), math.max(r1, r2)
+local ra1, ra2 = math.mean(rt1), math.mean(rt2)
+local rmax1, rmax2 = math.max(unpack(rt1)), math.max(unpack(rt2))
+local rmin1, rmin2 = math.min(unpack(rt1)), math.min(unpack(rt2))
+local min, max = math.min(rmax1, rmax2), math.max(rmax1, rmax2)
 local percent2 = max * 100 / min
 local percentFor1, percentFor2
 
-if min == r1 then
+if min == rmax1 then
 	percentFor1 = 100
 	percentFor2 = percent2
 else
@@ -102,5 +107,6 @@ else
 	percentFor2 = 100
 end
 
-print(name1 .. " = " .. r1 .. " (" .. topf(r1) .. ") second(s) (" .. percentFor1 .. "%) (" .. topf(percentFor1) .. "%)")
-print(name2 .. " = " .. r2 .. " (" .. topf(r2) .. ") second(s) (" .. percentFor2 .. "%) (" .. topf(percentFor2) .. "%)")
+print(name1 .. ": " .. rmax1 .. " (Min: " .. topf(rmin1) .. ", Average: " .. topf(ra1) .. ") second(s) (" .. percentFor1 .. "%) (" .. topf(percentFor1) .. "%)")
+print(name2 .. ": " .. rmax2 .. " (Min: " .. topf(rmin2) .. ", Average: " .. topf(ra2) .. ") second(s) (" .. percentFor2 .. "%) (" .. topf(percentFor2) .. "%)")
+os.execute("rundll32.exe cmdext.dll,MessageBeepStub")
